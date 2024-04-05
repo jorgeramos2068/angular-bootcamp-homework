c864189f25ab967eeb4bcc886d3daa434f4a98b4
Jorge Ramos <jorgeramos2068@gmail.com>
2024-04-04 23:29:52 -0600

    Add exercise for the use of pipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { Exercise07Component } from './components/exercise-07/exercise-07.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomepageComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'exercise-07',
+    component: Exercise07Component,
     pathMatch: 'full',
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Exercise07Component } from './components/exercise-07/exercise-07.component';
+import { CapitalizePipe } from './pipes/capitalize.pipe';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    Exercise07Component,
+    CapitalizePipe,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
